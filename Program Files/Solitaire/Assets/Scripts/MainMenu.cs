4127900119c81ed977e20a9e8c9903e9ee932bc3
Cameron Cosby <82902082+camcosby08@users.noreply.github.com>
2022-01-16 12:26:17 -0500

    MainMenu
    Basic functioning Main Menu for our game

---
+++ Program Files/Solitaire/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        Debug.Log("Loading Game Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting App");
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
